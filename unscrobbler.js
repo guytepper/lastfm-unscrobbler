bfbd44879c4cd82add17bfa084f76fe380418c58
Guy Tepper <guytepper@gmail.com>
2018-09-01 17:21:13 +0300

    Added deleteScrobbles method

--- unscrobbler.js
+++ unscrobbler.js
@@ -8,7 +8,7 @@ trackRows.forEach(row => {
   checkboxTableData.className = "chartlist-checkbox";
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  checkbox.name = "name";
+  checkbox.name = "unscrobble-checkbox";
   checkbox.value = "value";
   checkbox.id = "id";
   checkboxTableData.appendChild(checkbox);
@@ -21,6 +21,21 @@ moreMenu.forEach(menu => {
   const deleteButton = document.createElement("button");
   deleteButton.className = "mimic-link dropdown-menu-clickable-item";
   deleteButton.textContent = "Delete selected scrobbles";
+  deleteButton.onclick = deleteScrobbles;
   listItem.appendChild(deleteButton);
   menu.appendChild(listItem);
 });
+
+function deleteScrobbles() {
+  const checkboxes = document.getElementsByName("unscrobble-checkbox");
+  // Convert NodeList to an array.
+  const checkboxesArr = Array.from(checkboxes);
+  const checkedScrobbles = checkboxesArr.filter(node => node.checked);
+  checkedScrobbles.forEach(checkbox => {
+    const scrobbleRow = checkbox.parentNode.parentNode;
+    const deleteBtn = scrobbleRow.querySelector(
+      '[data-ajax-form-sets-state="deleted"]'
+    );
+    deleteBtn.click();
+  });
+}
